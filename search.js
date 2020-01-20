72fc0ae5d87bb1c3f31f418447a5a90768038e70
bajud124 <59533331+bajud124@users.noreply.github.com>
2020-01-20 18:32:41 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('.owl-lorem').owlCarousel({
+        nav: true,
+        dots: false,
+        margin: 10,
+        navText: ['<img class="arrow-left" src="images/left-white-arrow.png" >', '<img class="arrow-right" src="images/arrow-toggle.png" />'],
+        responsiveClass: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 2,
+            }
+        }
+    });
+});
